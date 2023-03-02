1b2eeb0d45ac42aa8b83675d7da6bc051bd39795
ijfields <76027894+ijfields@users.noreply.github.com>
2023-03-01 20:18:09 -0800

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const markdownElement = document.getElementById('markdown');
-
+/*
 function loadMarkdownFile(filename) {
   const url = filename + '.md';
 
@@ -20,6 +20,40 @@ function loadMarkdownFile(filename) {
       console.error(error);
     });
 }
+*/
+
+function loadMarkdown(filename) {
+  // Fetch the markdown file and convert it to HTML
+  fetch(filename)
+    .then(response => response.text())
+    .then(text => {
+      const converter = new showdown.Converter();
+      const html = converter.makeHtml(text);
+      markdown.innerHTML = html;
+    });
+}
+
+function loadMarkdownList() {
+  // Get all markdown files from the "markdown" directory
+  fetch('markdown/')
+    .then(response => response.text())
+    .then(text => {
+      // Use regex to extract the filenames from the directory listing
+      const regex = /<a href="(.*?)">.*?<\/a>/g;
+      let match;
+      while ((match = regex.exec(text))) {
+        const filename = match[1];
+        // If the file has the .md extension, add it to the list
+        if (filename.endsWith('.md')) {
+          const listItem = document.createElement('li');
+          listItem.innerHTML = `<a href="javascript:loadMarkdown('markdown/${filename}');">${filename}</a>`;
+          list.appendChild(listItem);
+        }
+      }
+    });
+}
+
+loadMarkdownList();
 
 window.addEventListener('hashchange', event => {
   const filename = event.newURL.split('#')[1];
